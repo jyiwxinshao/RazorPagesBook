22b60df3b113b9f8cd15345958dc883c9a1d5455
jyiwxinshao <148311603+jyiwxinshao@users.noreply.github.com>
2023-10-21 11:54:33 +0800

    Add files via upload

---
+++ RazorPagesBook105/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesBook105.Data;
+using RazorPagesBook.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorPagesBook105Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPagesBook105Context") ?? throw new InvalidOperationException("Connection string 'RazorPagesBook105Context' not found.")));
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
